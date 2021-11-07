ac8fa4315f248cdb71bc100f283cbcd73faf1301
salo1893 <salome.mueller@alansa.ch>
2021-11-07 11:48:44 +0100

    add initalMin, debug

--- src/OthelloAlgorithmAlphaBeta.java
+++ src/OthelloAlgorithmAlphaBeta.java
@@ -4,218 +4,261 @@ import java.util.LinkedList;
  * This interface defines the mandatory methods for game playing algorithms,
  * i.e., algorithms that take an <code>OthelloAlgorithm</code> and return a
  * suggested move for the player who has the move.
- * 
+ *
  * The algorithm only defines the search method. The heuristic evaluation of
  * positions is given by an <code>OthelloEvaluator</code> which is given to the
  * algorithm.
- * 
+ *
  * @author Henrik Bj&ouml;rklund
  */
 
 public class OthelloAlgorithmAlphaBeta implements OthelloAlgorithm {
 
-	OthelloEvaluator evaluator;
-	int depth;
-	protected boolean maxPlayer;
+    OthelloEvaluator evaluator;
+    int depth;
+    protected boolean maxPlayer;
 
-	public OthelloAlgorithmAlphaBeta(OthelloEvaluator evaluator) {
-		setEvaluator(evaluator);
-	}
+    public OthelloAlgorithmAlphaBeta(OthelloEvaluator evaluator) {
+        setEvaluator(evaluator);
+    }
 
-	/**
-	 * Sets the <code>OthelloEvaluator</code> the algorithm is to use for
-	 * heuristic evaluation.
-	 */
-	public void setEvaluator(OthelloEvaluator evaluator) {
-		this.evaluator = evaluator;
-	}
+    /**
+     * Sets the <code>OthelloEvaluator</code> the algorithm is to use for
+     * heuristic evaluation.
+     */
+    public void setEvaluator(OthelloEvaluator evaluator) {
+        this.evaluator = evaluator;
+    }
 
-	/**
-	 * Sets the current player.
-	 */
-	public void setPlayer(boolean maxPlayer) {
-		this.maxPlayer = maxPlayer;
-	}
+    /**
+     * Sets the current player.
+     */
+    public void setPlayer(boolean maxPlayer) {
+        this.maxPlayer = maxPlayer;
+    }
 
-	/**
-	 * Returns the <code>OthelloAction</code> the algorithm considers to be the
-	 * best move, using alpha-beta pruning.
-	 */
-	public OthelloAction evaluate(OthelloPosition position) {
+    /**
+     * Returns the <code>OthelloAction</code> the algorithm considers to be the
+     * best move, using alpha-beta pruning.
+     */
+    public OthelloAction searchAction(OthelloPosition position) {
+        return maxPlayer ? initialMaxValue(position) : initialMinValue(position);
+    }
 
-		LinkedList<OthelloAction> moves = position.getMoves();
+    private OthelloAction initialMaxValue(OthelloPosition position) {
 
-		// if no moves are possible, return pass action
-		if (moves.size() == 0) {
-			return new OthelloAction("pass");
-		}
+        LinkedList<OthelloAction> moves = position.getMoves();
 
-		OthelloAction action = new OthelloAction(-1,-1);
-		action.setValue(Integer.MIN_VALUE);
-		int alpha = Integer.MIN_VALUE;
-		int beta = Integer.MAX_VALUE;
+        // if no moves are possible, return pass action
+        if (moves.size() == 0) {
+            return new OthelloAction("pass");
+        }
 
-		OthelloPosition new_position;
+        OthelloAction action = new OthelloAction(-1, -1);
+        action.setValue(Integer.MIN_VALUE);
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
 
-		// perform maxValue, updating the action
-		for (OthelloAction move: moves) {
-			try {
-				// create new position, apply the action (may throw exception)
-				new_position = position.clone().makeMove(move);
+        OthelloPosition new_position;
 
-				// call minValue on the new position
-				int min = minValue(new_position, alpha, beta, depth - 1);
+        // perform maxValue, updating the action
+        for (OthelloAction move : moves) {
+            try {
+                // create new position, apply the action (may throw exception)
+                new_position = position.makeMove(move);
 
-				// update value and alpha, if possible
-				if (min > action.getValue()) {
+                // call minValue on the new position
+                int newValue = minValue(new_position, alpha, beta, depth - 1);
 
-					action.setValue(min);
-					action.setRow(move.getRow());
-					action.setColumn(move.getColumn());
+                // update value and alpha, if possible
+                if (newValue > action.getValue()) {
 
-					if (alpha < min) {
-						alpha = min;
-					}
-				}
-			} catch (IllegalMoveException e) {
-				// makeMove could throw an exception TODO: how to handle exception
-				e.printStackTrace();
-			}
-		}
+                    action.setValue(newValue);
+                    action.setRow(move.getRow());
+                    action.setColumn(move.getColumn());
 
-		return action;
-	}
+                    if (alpha < newValue) {
+                        alpha = newValue;
+                    }
+                }
+            } catch (IllegalMoveException e) {
+                // makeMove could throw an exception TODO: how to handle exception
+                e.printStackTrace();
+            }
+        }
 
-	/**
-	 * MaxValue method of alpha-beta pruning. Returns the value of its child with the biggest value, or own value if a
-	 * leave or if the iteration depth is reached.
-	 * @param position OthelloPosition of the node
-	 * @param alpha alpha value
-	 * @param beta beta value
-	 * @param depth iteration depth
-	 * @return maxValue that can be achieved, or value of the leave
-	 */
-	private int maxValue(OthelloPosition position, int alpha, int beta, int depth) {
-		if (depth == 0) {
-			// if depth is reached return heuristic value
-			if (position.toMove() != maxPlayer)
-				position.nextMove();
-			return evaluator.evaluate(position);
-		}
+        return action;
+    }
 
-		LinkedList<OthelloAction> moves = position.getMoves();
+    private OthelloAction initialMinValue(OthelloPosition position) {
 
-		// if no moves are possible, the game is either over or the player must pass
-		if (moves.size() == 0) {
-			position.nextMove();
+        LinkedList<OthelloAction> moves = position.getMoves();
 
-			if (position.getMoves().size() == 0) {
-				// if neither of the players can move, the game is over; return heuristic value (score in this case)
-				if (position.toMove() != maxPlayer)
-					position.nextMove();
-				return evaluator.evaluate(position);
-			}
+        // if no moves are possible, return pass action
+        if (moves.size() == 0) {
+            return new OthelloAction("pass");
+        }
 
-			// else pass
-			return minValue(position, alpha, beta, depth -1);
-		}
+        OthelloAction action = new OthelloAction(-1, -1);
+        action.setValue(Integer.MAX_VALUE);
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
 
-		int value = Integer.MIN_VALUE;
-		OthelloPosition new_position;
+        OthelloPosition new_position;
 
-		for (OthelloAction action: moves) {
-			try {
-				// create new position, apply the action (may throw exception)
-				new_position = position.clone().makeMove(action);
+        // perform maxValue, updating the action
+        for (OthelloAction move : moves) {
+            try {
+                // create new position, apply the action (may throw exception)
+                new_position = position.makeMove(move);
 
-				// call minValue on the new position
-				int min = minValue(new_position, alpha, beta, depth - 1);
+                // call maxValue on the new position
+                int newValue = maxValue(new_position, alpha, beta, depth - 1);
 
-				// update value and alpha, if possible
-				if (min > value) {
-					value = min;
-					if (alpha < value) {
-						alpha = value;
-						if (alpha >= beta) {
-							return value;
-						}
-					}
-				}
-			} catch (IllegalMoveException e) {
-				// makeMove could throw an exception TODO: how to handle exception
-				e.printStackTrace();
-			}
-		}
+                // update value and alpha, if possible
+                if (newValue < action.getValue()) {
 
-		return value;
-	}
+                    action.setValue(newValue);
+                    action.setRow(move.getRow());
+                    action.setColumn(move.getColumn());
 
-	/**
-	 * MinValue method of alpha-beta pruning. Returns the value of its child with the smallest value, or own value if a
-	 * leave or if the iteration depth is reached.
-	 * @param position OthelloPosition of the node
-	 * @param alpha alpha value
-	 * @param beta beta value
-	 * @param depth iteration depth
-	 * @return minValue that can be achieved, or value of the leave
-	 */
-	private int minValue(OthelloPosition position, int alpha, int beta, int depth) {
-		if (depth == 0) {
-			// if depth is reached return heuristic value
-			if (position.toMove() != maxPlayer)
-				position.nextMove();
-			return evaluator.evaluate(position);
-		}
+                    if (beta > newValue) {
+                        beta = newValue;
+                    }
+                }
+            } catch (IllegalMoveException e) {
+                // makeMove could throw an exception TODO: how to handle exception
+                e.printStackTrace();
+            }
+        }
 
-		LinkedList<OthelloAction> moves = position.getMoves();
+        return action;
+    }
 
-		// if no moves are possible, the game is either over or the player must pass
-		if (moves.size() == 0) {
-			position.nextMove();
+    /**
+     * MaxValue method of alpha-beta pruning. Returns the value of its child with the biggest value, or own value if a
+     * leave or if the iteration depth is reached.
+     *
+     * @param position OthelloPosition of the node
+     * @param alpha    alpha value
+     * @param beta     beta value
+     * @param depth    iteration depth
+     * @return maxValue that can be achieved, or value of the leave
+     */
+    private int maxValue(OthelloPosition position, int alpha, int beta, int depth) {
+        if (depth == 0) {
+            // if depth is reached return heuristic value
+            return evaluator.evaluate(position);
+        }
 
-			if (position.getMoves().size() == 1) {
-				// if neither of the players can move, the game is over; return heuristic value
-				if (position.toMove() != maxPlayer)
-					position.nextMove();
-				return evaluator.evaluate(position);
-			}
+        LinkedList<OthelloAction> moves = position.getMoves();
 
-			// else pass
-			return maxValue(position, alpha, beta, depth -1);
-		}
+        // if no moves are possible, the game is either over or the player must pass
+        if (moves.size() == 0) {
+            position.nextMove();
 
-		int value = Integer.MAX_VALUE;
-		OthelloPosition new_position;
+            if (position.getMoves().size() == 0) {
+                // if neither of the players can move, the game is over; return the score
+                return position.score();
+            }
 
-		for (OthelloAction action: moves) {
-			try {
-				// create new position, apply the action (may throw exception)
-				new_position = position.clone().makeMove(action);
+            // else pass
+            return minValue(position, alpha, beta, depth - 1);
+        }
 
-				// call minValue on the new position
-				int max = maxValue(new_position, alpha, beta, depth - 1);
+        int value = Integer.MIN_VALUE;
+        OthelloPosition new_position;
 
-				// update value and beta, if possible
-				if (max < value) {
-					value = max;
-					if (beta > value) {
-						beta = value;
-						if (alpha >= beta) {
-							return value;
-						}
-					}
-				}
-			} catch (IllegalMoveException e) {
-				// makeMove could throw an exception
-				e.printStackTrace();
+        for (OthelloAction action : moves) {
+            try {
+                // create new position, apply the action (may throw exception)
+                new_position = position.makeMove(action);
+
+                // call minValue on the new position
+                int min = minValue(new_position, alpha, beta, depth - 1);
+
+                // update value and alpha, if possible
+                if (min > value) {
+                    value = min;
+                    if (alpha < value) {
+                        alpha = value;
+                        if (alpha >= beta) {
+                            return value;
+                        }
+                    }
+                }
+            } catch (IllegalMoveException e) {
+                // makeMove could throw an exception TODO: how to handle exception
+                e.printStackTrace();
+            }
+        }
+
+        return value;
+    }
+
+    /**
+     * MinValue method of alpha-beta pruning. Returns the value of its child with the smallest value, or own value if a
+     * leave or if the iteration depth is reached.
+     *
+     * @param position OthelloPosition of the node
+     * @param alpha    alpha value
+     * @param beta     beta value
+     * @param depth    iteration depth
+     * @return minValue that can be achieved, or value of the leave
+     */
+    private int minValue(OthelloPosition position, int alpha, int beta, int depth) {
+        if (depth == 0) {
+            // if depth is reached return heuristic value
+            return evaluator.evaluate(position);
+        }
+
+        LinkedList<OthelloAction> moves = position.getMoves();
+
+        // if no moves are possible, the game is either over or the player must pass
+        if (moves.size() == 0) {
+            position.nextMove();
+
+            if (position.getMoves().size() == 1) {
+                // if neither of the players can move, the game is over; return the score
+                return position.score();
+            }
+
+            // else pass
+            return maxValue(position, alpha, beta, depth - 1);
+        }
+
+        int value = Integer.MAX_VALUE;
+        OthelloPosition new_position;
+
+        for (OthelloAction action : moves) {
+            try {
+                // create new position, apply the action (may throw exception)
+                new_position = position.makeMove(action);
+
+                // call minValue on the new position
+                int max = maxValue(new_position, alpha, beta, depth - 1);
+
+                // update value and beta, if possible
+                if (max < value) {
+                    value = max;
+                    if (beta > value) {
+                        beta = value;
+                        if (alpha >= beta) {
+                            return value;
+                        }
+                    }
+                }
+            } catch (IllegalMoveException e) {
+                // makeMove could throw an exception
+                e.printStackTrace();
 				// TODO: how to handle exception
-			}
-		}
+            }
+        }
 
-		return value;
-	}
+        return value;
+    }
 
-	public void setSearchDepth(int depth) {
-		this.depth = depth;
-	}
+    public void setSearchDepth(int depth) {
+        this.depth = depth;
+    }
 }
